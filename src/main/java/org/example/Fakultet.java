79169bc9bc4c81efab0670f385acbb46d5f90228
Naci Gokmen <nacigokmen99@gmail.com>
2023-09-07 23:57:23 +0200

    Fakultet

--- src/main/java/org/example/Fakultet.java
+++ src/main/java/org/example/Fakultet.java
@@ -2,6 +2,31 @@ package org.example;
 
 public class Fakultet {
     public static void main(String[] args) {
-        
+       // System.out.println(fakultet(5));
+        System.out.println(fakultetRec(5));
+    }
+
+    public static int fakultet(int n){
+        if(n==0) return 1;
+
+
+        int multip=1;
+        if(n>=1){
+            for (int i = 1; i <n+1 ; i++) {
+                multip*=i;
+            }
+        }
+        return multip;
+    }
+
+
+    public static int fakultetRec(int n){
+
+        if(n==0 || n==1) return 1;
+
+        else {
+            return n*fakultetRec(n-1);
+        }
+
     }
 }
